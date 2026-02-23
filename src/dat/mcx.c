d4989bb63454f3f145a9a81cd50329debd6e909e
Quinn <quinn@thepigeongenerator.xyz>
2026-02-23 13:23:09 +0100

    Start reworking mcx parsers, deleting a lot of code.
    I removed a lot of logic, since I am just focussing on getting the size
    computations right at the moment.

--- src/dat/mcx.c
+++ src/dat/mcx.c
@@ -1,198 +1,37 @@
-/* Copyright (C)2025 MCA-Selector-Lite
+/* Copyright (C)2025-2026 MCA-Selector-Lite
  * Licensed under GPL-2.0-only. For further information,
  * view `git log`, and the COPYING and CONTRIBUTORS files
  * at www.github.com/thepigeongenerator/mcaselector-lite. */
 #include "mcx.h"
 
-#include <archive.h>
-#include <assert.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
 #include "../endian.h"
 #include "../types.h"
-#include "../util/error.h"
 
-#define SECTOR 0x1000 // sector size
-#define TABLE  0x800  // table (total) element count
-#define CHUNKS 0x400  // amount of chunks in a file
-
-enum mcx_compression {
-	MCX_COMPRESSION_GZIP   = 0x01,
-	MCX_COMPRESSION_ZLIB   = 0x02,
-	MCX_COMPRESSION_NONE   = 0x03,
-	MCX_COMPRESSION_LZ4    = 0x04,
-	MCX_COMPRESSION_CUSTOM = 0x7F,
-};
-
-/* first 4 bytes is an s32 indicating remaining bytes, the following byte defines the compression scheme */
-static int mcx_loadchunk(const u8 *restrict buf, const be32 *restrict table, int idx)
+usize mcx_table_calcsize(const be32 *tbl)
 {
-	const u8 *chunk = buf + (cvt_be32toh(table[idx]) >> 8) * SECTOR;
+	/* BUG: Sometimes still some sectors too few?
+	 * Maybe old/unused data? Why not truncate? */
+	const be32 *end = tbl + MCX_TABLE_LEN;
 
-	be32 tmp;
-	memcpy(&tmp, chunk, 4);
-	s32 len = cvt_be32toh(tmp);
-	chunk += 4;
-
-	struct archive *archive = archive_read_new();
-	archive_read_support_format_raw(archive);
-	switch (*chunk) {
-	case MCX_COMPRESSION_GZIP: /* fall-through; ZLIB is handled under the GZIP filter */
-	case MCX_COMPRESSION_ZLIB:   archive_read_support_filter_gzip(archive); break;
-	case MCX_COMPRESSION_NONE:   archive_read_support_filter_none(archive); break;
-	case MCX_COMPRESSION_LZ4:    archive_read_support_filter_lz4(archive); break;
-	case MCX_COMPRESSION_CUSTOM: archive_read_support_filter_all(archive); break;
-	default:                     fatal("compression type of '%i' is unsupported!", *chunk);
+	/* We can get away with comparing without masking,
+	 * Since the sector data is the least significant byte,
+	 * and thus won't weigh into our results. */
+	u32 max = 0, tmp;
+	while (tbl < end) {
+		tmp = cvt_be32toh(*tbl++);
+		max = max < tmp ? tmp : max;
 	}
+	return ((max >> 8) + (max & 0xFF)) * MCX_SECTOR;
+}
 
-	if (archive_read_open_memory(archive, chunk, len) != ARCHIVE_OK) {
-		error("failed to decompress %i bytes of compression type %i", len, *chunk);
-		return 1;
+usize mcx_table_minsize(const be32 *tbl)
+{
+	const be32 *end = tbl + MCX_TABLE_LEN;
+
+	usize sum = 0;
+	while (tbl < end) {
+		const u8 *m = (const u8 *)tbl;
+		sum += m[3];
 	}
-
-	struct archive_entry *entry;
-	if (archive_read_next_header(archive, &entry) != ARCHIVE_OK) {
-		error("failed to decompress %i bytes of compression type %i", len, *chunk);
-		return 1;
-	}
-
-	ssize size = -1;
-	for (;;) {
-		// TODO: handle buffer
-		// size = archive_read_data(archive, , );
-		if (size < 0) {
-			error("failed to decompress %i bytes of compression type %i", len, *chunk);
-			return 1;
-		}
-		if (size == 0)
-			break;
-		// TODO: handle data
-	}
-
-	return 0;
-}
-
-/* Moves chunks `src_s` to `src_e` (inclusive) from `src`, back onto `dst`. */
-static void mvchunks(u8 *dst, u8 *src, be32 *restrict table, int src_s, int src_e)
-{
-	assert(src > dst);
-	uintptr len = src - dst; // acquire the amount of bytes that we shall move
-	assert(!(len % SECTOR));
-
-	// count how many bytes we need to move, whilst updating location data
-	u32   tmp;
-	usize blen = 0;
-	for (src_s++; src_s <= src_e; src_s++) {
-		tmp = cvt_be32toh(table[src_s]);
-		blen += (tmp & 0xFF) * SECTOR;
-		tmp -= (len / SECTOR) << 8;
-		table[src_s] = cvt_htobe32(tmp);
-	}
-	memmove(dst, src, blen);
-}
-
-/* Deletes chunk `sidx` by moving chunks up to `eidx` back over `sidx` in `buf`.
- * `rmb` is an optional additional offset that can be applied, and signifies bytes already removed.
- * Returns the bytes removed by this function. */
-static usize delchunk(u8 *restrict buf, be32 *restrict table, usize rmb, int sidx, int eidx)
-{
-	// load the table data
-	usize slen, bidx, blen;
-	slen = cvt_be32toh(table[sidx]) & 0xFF;          // acquire the sector length of the chunk
-	bidx = (cvt_be32toh(table[sidx]) >> 8) * SECTOR; // acquire and compute the byte offset the chunk starts at
-	blen = slen * SECTOR;                            // compute the byte length of the chunk
-
-	// reset the table data
-	table[sidx] = 0;
-	// WARN: might need to zero-out the timestamp instead
-	table[sidx + CHUNKS] = cvt_htobe32(time(NULL));
-
-	// move the succeeding chunks over the deleted chunk
-	u8 *dst = buf + bidx - rmb;
-	u8 *src = buf + bidx + blen;
-	mvchunks(dst, src, table, sidx, eidx - 1);
-	return blen;
-}
-
-/* Call `delchunk` with the parameters and some defaults. Ensuring the table is copied correctly as well.
- * This is done instead of `delchunk` being globally linked, because
- * `delchunk` requests more specific parameters, which is confusing outside this module. */
-usize mcx_delchunk(u8 *restrict buf, int chunk)
-{
-	be32 table[TABLE];
-	memcpy(table, buf, sizeof(table));
-	usize res = delchunk(buf, table, 0, chunk, CHUNKS);
-	memcpy(buf, table, sizeof(table));
-	return res;
-}
-
-usize mcx_delchunk_range(u8 *restrict buf, int start, int end)
-{
-	assert(start < end && end < CHUNKS);
-	be32 table[TABLE];
-	memcpy(table, buf, sizeof(table));
-	u8 *dst = buf + (cvt_be32toh(table[start]) >> 8) * SECTOR;
-	u8 *src = buf + (cvt_be32toh(table[end]) >> 8) * SECTOR;
-	// BUG: We are selecting the wrong byte for this.
-	src += (cvt_be32toh(table[end]) & 0xFF) * SECTOR;
-
-	// zeroes-out the chunk data within this range. (and set the timestamp)
-	// WARN: May need to zero-out the timestamp instead.
-	be32 ts = cvt_htobe32(time(NULL));
-	for (int i = start; i <= end; i++) {
-		table[i]          = 0;
-		table[i + CHUNKS] = ts;
-	}
-
-	// move the remaining chunks down
-	// BUG: Chunks may not necessarily be stored in order.
-	if (end < (CHUNKS - 1))
-		mvchunks(dst, src, table, end, (CHUNKS - 1));
-	memcpy(buf, table, sizeof(table));
-	return src - dst;
-}
-
-/* comparer function for to be inputted into `qsort` to compare two */
-static int cmp_chunkids(const void *restrict x, const void *restrict y)
-{
-	u16 x2 = *(u16 *)x;
-	u16 y2 = *(u16 *)y;
-	return (x2 > y2) - (x2 < y2);
-}
-
-/* Sorts the chunks marked for deletion from smallest to greatest index.
- * Then performs the deletion in this order. Making sure to only update the chunks up to the next. */
-usize mcx_delchunk_bulk(u8 *restrict buf, const u16 *restrict chunks, int chunkc)
-{
-	// ensure the chunks ids we're working on are sorted from least to greatest
-	u16 chunkids[chunkc + 1];
-	memcpy(chunkids, chunks, chunkc);
-	qsort(chunkids, chunkc, sizeof(int), cmp_chunkids);
-	chunkids[chunkc] = CHUNKS; // set the spare chunk to the max chunks, so the rest of the chunks are moved
-
-	u32 table[TABLE];
-	memcpy(table, buf, sizeof(table));
-
-	usize rmb = 0;
-	for (int i = 0; i < chunkc; i++)
-		rmb += delchunk(buf, table, rmb, chunkids[i], chunkids[i + 1]);
-
-	memcpy(buf, table, sizeof(table));
-	return rmb;
-}
-
-/* Sum together the 4th byte in each location integer to compute the sector size of all chunks.
- * Multiplying by `SECTOR`, and adding the size of the table itself. */
-usize mcx_calcsize(const u8 *restrict buf)
-{
-	/* NOTE: This can be optimised if chunks are always
-	 * in order. I.e. we can guarantee that the last chunk in
-	 * the table has the greatest offset. */
-	usize size = 0;
-	for (uint i = 0; i < CHUNKS; i++)
-		size += *(buf + (i * 4) + 3);
-	return (size * SECTOR) + (TABLE * 4);
+	return sum * MCX_SECTOR + MCX_TABLES;
 }
--- src/dat/mcx.h
+++ src/dat/mcx.h
@@ -1,40 +1,26 @@
-/* Copyright (C)2025 MCA-Selector-Lite
+/* Copyright (C)2025-2026 MCA-Selector-Lite
  * Licensed under GPL-2.0-only. For further information,
  * view `git log`, and the COPYING and CONTRIBUTORS files
  * at www.github.com/thepigeongenerator/mcaselector-lite. */
 #pragma once
 
-#include <stdlib.h>
-
 #include "../atrb.h"
 #include "../types.h"
 
+#define MCX_SECTOR     0x1000 /* Size in bytes for a sector. */
+#define MCX_TABLE      0x1000 /* Size in bytes for one table. */
+#define MCX_TABLES     0x2000 /* Size in bytes for the tables. */
+#define MCX_TABLE_LEN  0x400  /* Amount of elements within one table. */
+#define MCX_TABLES_LEN 0x800  /* Amount of elements within the tables. */
 
-/* contains chunk metadata */
-struct mcx_chunk {
-	usize idx;  // byte offset for start of chunk data
-	u32   len;  // byte length of chunk (+ padding)
-	u32   time; // modification time in epoch seconds
-};
+/* Computes the size in bytes of the entire .mcX file,
+ * according to the table.
+ * It is assumed that tbl points to a valid address
+ * with sufficiently allocated memory. */
+usize mcx_table_calcsize(const be32 *tbl);
 
-/* Deletes a single chunk (`chunk`) out of `buf`.
- * The chunk's location data shall become `0`, and timestamp data the current time.
- * All succeeding chunks shall be moved back, freeing space.
- * Returns the amount of bytes removed. */
-usize mcx_delchunk(u8 *restrict buf, int chunk) NONNULL((1));
-
-/* Deletes the range defined by `start`—`end` (inclusive) of chunks out of `buf`.
- * The chunk's location data shall become `0`, and timestamp data the current time.
- * All succeeding chunks shall be moved back, freeing space.
- * Returns the amount of bytes removed */
-usize mcx_delchunk_range(u8 *restrict buf, int start, int end) NONNULL((1));
-
-/* Deletes a `chunkc` chunks from `chunks` out of `buf`.
- * If the `chunks` indices are known to be sequential, i.e. have a constant difference of `1`, `mcx_delchunk_range` should be preferred.
- * The chunk's location data shall become `0`, and timestamp data the current time.
- * All succeeding chunks shall be moved back, freeing space.
- * Returns the amount of bytes removed */
-usize mcx_delchunk_bulk(u8 *restrict buf, const u16 *restrict chunks, int chunkc) NONNULL((1, 2));
-
-/* Computes the byte size of the `*.mcX` file in `buf` and returns it. */
-usize mcx_calcsize(const u8 *restrict buf) NONNULL((1)) PURE;
+/* Computes the minimum size in bytes required for the entire .mcX file,
+ * according to the table.
+ * It is assumed that tbl points to a valid address
+ * with sufficiently allocated memory. */
+usize mcx_table_minsize(const be32 *tbl);
